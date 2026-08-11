7a3a86a19c5746670bba5bc137e4e0a13f84c002
vicke <vickenezik@hotmail.com>
2026-08-10 21:44:59 -0400

    Better mobile support

--- sideNav.js
+++ sideNav.js
@@ -2,9 +2,11 @@ const text = document.getElementById("yes");
 
 function openNav() {
     var screenWidth = window.innerWidth
-    let openpos = (1.69459e-9) * screenWidth**4 - 0.00000717135 * screenWidth**3 + 0.0111188 * screenWidth**2 - 6.47938 * screenWidth + 1414.1067;
+    let openpos = (8.88074 * 10**-10)*Math.pow(screenWidth, 4) - 0.00000462262*Math.pow(screenWidth, 3) + 0.0087766*Math.pow(screenWidth, 2) - 6.19803*screenWidth + 1718.77043
     document.documentElement.style.setProperty('--swordPosition', ''+openpos+'px');
-    
+    let openposPhone = -0.00000418598*Math.pow(screenWidth, 3) + 0.00526292*Math.pow(screenWidth, 2) - 1.0987*screenWidth - 66.25771
+    document.documentElement.style.setProperty('--swordPositionPhone', ''+openposPhone+'px');
+
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginRight = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -24,11 +26,10 @@ function closeNav() {
 }
 
 (function() {
-    
     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-    link.type = 'image/x-icon';
+    link.type = 'image/png';
     link.rel = 'shortcut icon';
-    link.href = 'https://vickenezik.com/images/favicon.ico';
+    link.href = '/images/favicon.png';
     document.getElementsByTagName('head')[0].appendChild(link);
 })();
 
